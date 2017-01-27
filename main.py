8e8730fc36899803cfad406f8228a7c22e769fb4
Brendan <mcb0193@comcast.net>
2017-01-27 16:07:30 -0600

    Set up HTML for form with basic validation

--- main.py
+++ main.py
@@ -16,10 +16,47 @@
 #
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+class MainHandler(webapp2.RequestHandler):	
+	def get(self):
+		page_content = """
+			<html>
+				<head>
+					<title>User Signup</title>
+					<link rel="stylesheet" href="css/style.css" type="text/css" />
+				</head>
+				<body>
+					<h1>Register a user account</h1>
+					<p>(&#42; = required field)</p>
+					<form method="post" action="/">
+						<div class="form-field">
+							<label for="user">Username&#42;</label>
+							<input type="text" id="user" name="username" required />
+						</div>	
+						<div class="form-field">
+							<label for="mail">Email</label>
+							<input type="email" id="mail" name="email" />
+						</div>
+						<div class="form-field">
+							<label for="pass">Password&#42;</label>
+							<input type="password" id="pass" name="password" required />
+						</div>
+						<div class="form-field">
+							<label for="pass-confirm">Confirm Password&#42;</label>
+							<input type="password" id="pass-confirm" name="password" required />
+						</div>
+						<div class="form-field">
+							<input type="submit" />
+						</div>
+					</form>
+				</body>
+			</html>
+		"""
+		
+		self.response.write(page_content)
+		
+	def post(self):
+		self.redirect('/')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+	('/', MainHandler)
 ], debug=True)
